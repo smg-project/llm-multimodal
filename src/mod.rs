34342ce68fc58b2f91191548be387b8cc1455f41
Simo Lin <linsimo.mark@gmail.com>
2026-01-19 12:44:51 -0800

    fix: remove dead code (lightseekorg/smg#60)

--- src/mod.rs
+++
@@ -1,6 +0,0 @@
-//! Re-exports from the llm-multimodal crate.
-//!
-//! This module provides backwards-compatible access to multimodal functionality
-//! that has been extracted to the standalone `llm-multimodal` crate.
-
-pub use llm_multimodal::*;
